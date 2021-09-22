233ebc001dada1cc7b12d6223fb122384bca8b4f
OctaviaOZ <19343878+OctaviaOZ@users.noreply.github.com>
2021-09-22 15:47:35 +0300

    Update medium.py

--- medium.py
+++ medium.py
@@ -50,4 +50,38 @@ class Solution:
                     i = nums[i]
                     out+=1
                     r = max(out, r)  
-        return r  
+        return r 
+    
+    def sortArray(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 1:
+            return nums
+
+        mid = len(nums) // 2
+        l_arr = nums[:mid]
+        r_arr = nums[mid:]
+        self.sortArray(l_arr)
+        self.sortArray(r_arr)
+        self.merge(l_arr, r_arr, nums)
+        return nums
+
+
+    def merge(self, l_arr, r_arr, arr):
+        i = j = k = 0
+        while i < len(l_arr) and j < len(r_arr):
+            if l_arr[i] < r_arr[j]:
+                arr[k] = l_arr[i]
+                i += 1
+            else:
+                arr[k] = r_arr[j]
+                j += 1
+            k += 1
+        
+        while i < len(l_arr):
+            arr[k] = l_arr[i]
+            i += 1
+            k += 1
+
+        while j < len(r_arr):
+            arr[k] = r_arr[j]
+            j += 1
+            k += 1
